96d0417ae69b4cf797e8f6da05ead9112caada4e
Ken Nguyen <kentmn@gmail.com>
2021-01-22 02:46:07 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '416181799590080',
+        clientSecret: '754a6c397c1289194c849cb394bd6049'
+    }
 }
